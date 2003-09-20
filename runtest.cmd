f92e68331692daaa7bb7583f2206e597a580d4c1
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2003-09-20 11:30:46 +0000

    revert bad checkin
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_2_1/ejbca@1155 37aeb1de-b14b-0410-9a0e-82b28170d228

--- runtest.cmd
+++ runtest.cmd
@@ -12,21 +12,21 @@ rem set TEST_CP=.;..\..\lib\weblogic.jar;..\..\lib\junit.jar;..\..\lib\log4j-1.2
 
 
 echo Testing utils
-rem java -cp %TEST_CP% se.anatom.ejbca.util.junit.TestRunner
+java -cp %TEST_CP% se.anatom.ejbca.util.junit.TestRunner
 echo Testing messages
 rem java -cp %TEST_CP% se.anatom.ejbca.protocol.junit.TestRunner
 echo Testing ra
-rem java -cp %TEST_CP% se.anatom.ejbca.ra.junit.TestRunner
+java -cp %TEST_CP% se.anatom.ejbca.ra.junit.TestRunner
 echo Testing ca.auth
-rem java -cp %TEST_CP% se.anatom.ejbca.ca.auth.junit.TestRunner
+java -cp %TEST_CP% se.anatom.ejbca.ca.auth.junit.TestRunner
 echo Testing ca.store
 java -cp %TEST_CP% se.anatom.ejbca.ca.store.junit.TestRunner
 echo Testing ca.sign
-rem java -cp %TEST_CP% se.anatom.ejbca.ca.sign.junit.TestRunner
+java -cp %TEST_CP% se.anatom.ejbca.ca.sign.junit.TestRunner
 echo Testing ca.crl
-rem java -cp %TEST_CP% se.anatom.ejbca.ca.crl.junit.TestRunner
+java -cp %TEST_CP% se.anatom.ejbca.ca.crl.junit.TestRunner
 echo Testing batch
-rem java -cp %TEST_CP% se.anatom.ejbca.batch.junit.TestRunner
+java -cp %TEST_CP% se.anatom.ejbca.batch.junit.TestRunner
 
 cd ..\..
 
