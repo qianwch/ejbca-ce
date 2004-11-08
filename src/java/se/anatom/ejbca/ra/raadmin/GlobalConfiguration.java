3d200272aefbc74b639cb0ea0b224f1c5764648c
herrvendil <USER@primekey.com>
2004-11-08 09:01:13 +0000

    Updated version
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_0/ejbca@2077 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/ra/raadmin/GlobalConfiguration.java
+++ src/java/se/anatom/ejbca/ra/raadmin/GlobalConfiguration.java
@@ -18,14 +18,14 @@ import se.anatom.ejbca.util.UpgradeableDataHashMap;
 /**
  * This is a  class containing global configuration parameters.
  *
- * @version $Id: GlobalConfiguration.java,v 1.12.2.2 2004-08-08 11:57:07 anatom Exp $
+ * @version $Id: GlobalConfiguration.java,v 1.12.2.3 2004-11-08 09:01:13 herrvendil Exp $
  */
 public class GlobalConfiguration extends UpgradeableDataHashMap implements java.io.Serializable {
 
     // Default Values
     public static final float LATEST_VERSION = 1;
     
-    public static final String EJBCA_VERSION = "EJBCA 3.0.3";
+    public static final String EJBCA_VERSION = "EJBCA 3.0.4";
 
 
     // Entries to choose from in userpreference part, defines the size of data to be displayed on one page.
