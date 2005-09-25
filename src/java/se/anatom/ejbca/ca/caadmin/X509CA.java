324bf3261c4aaf89652e1a4253326e92efa9af59
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2005-09-25 14:18:53 +0000

    ECA-155: use catoken.getProvider() to sign the response with the correct provider
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_1/ejbca@2615 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/se/anatom/ejbca/ca/caadmin/X509CA.java
+++ src/java/se/anatom/ejbca/ca/caadmin/X509CA.java
@@ -105,7 +105,7 @@ import se.anatom.ejbca.util.StringTools;
  * X509CA is a implementation of a CA and holds data specific for Certificate and CRL generation 
  * according to the X509 standard. 
  *
- * @version $Id: X509CA.java,v 1.41.2.1 2005-08-02 11:38:10 anatom Exp $
+ * @version $Id: X509CA.java,v 1.41.2.2 2005-09-25 14:18:53 anatom Exp $
  */
 public class X509CA extends CA implements Serializable {
 
@@ -581,7 +581,7 @@ public class X509CA extends CA implements Serializable {
                       log.debug("<extendedService(super)");
                       return super.extendedService(request);                      
                   }
-                  BasicOCSPResp ocspresp = ocsprespgen.generate(sigAlg, pk, chain, new Date(), "BC" );
+                  BasicOCSPResp ocspresp = ocsprespgen.generate(sigAlg, pk, chain, new Date(), getCAToken().getProvider() );
                   returnval = new OCSPCAServiceResponse(ocspresp, chain == null ? null : Arrays.asList(chain));              
               } catch (IllegalKeyStoreException ike) {
                   throw new ExtendedCAServiceRequestException(ike);
