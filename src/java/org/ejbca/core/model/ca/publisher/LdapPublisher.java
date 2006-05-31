00b959b563de4ce6853ceacf6f1287e2523a664c
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2006-05-31 11:25:33 +0000

    improve error logging
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_2/ejbca@3076 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/model/ca/publisher/LdapPublisher.java
+++ src/java/org/ejbca/core/model/ca/publisher/LdapPublisher.java
@@ -49,7 +49,7 @@ import com.novell.ldap.LDAPModification;
 /**
  * LdapPublisher is a class handling a publishing to various v3 LDAP catalouges.  
  *
- * @version $Id: LdapPublisher.java,v 1.3 2006-02-08 11:18:50 anatom Exp $
+ * @version $Id: LdapPublisher.java,v 1.3.2.1 2006-05-31 11:24:36 anatom Exp $
  */
 public class LdapPublisher extends BasePublisher {
 	 	
@@ -386,8 +386,8 @@ public class LdapPublisher extends BasePublisher {
                 log.debug("Added object: " + dn + " successfully.");                
             }
         } catch (LDAPException e) {
-            log.error("Error storing CRL (" + getCRLAttribute() + ") in LDAP (" + getCAObjectClass() + "): ", e);
-            throw new PublisherException("Error storing CRL (" + getCRLAttribute() + ") in LDAP (" + getCAObjectClass() + "): ");                        
+            log.error("Error storing CRL (" + getCRLAttribute() + ") in LDAP (" + getCAObjectClass() + ") for DN (" + dn + "): ", e);
+            throw new PublisherException("Error storing CRL (" + getCRLAttribute() + ") in LDAP (" + getCAObjectClass() + ") for DN (" + dn + ").");                        
         } catch (UnsupportedEncodingException e) {
             log.error("Can't decode password for LDAP login: "+getLoginPassword(), e);
             throw new PublisherException("Can't decode password when storing (" + getCRLAttribute() + ") in LDAP (" + getCAObjectClass() + ") for DN (" + dn + ").");            
