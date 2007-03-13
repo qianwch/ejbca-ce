8edf9e853ac6fefae0e096a01dd874502dbfe342
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2007-03-13 12:42:40 +0000

    ECA-451: catch errors that only occur in weblogic
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_4/ejbca@4250 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/ejb/services/ServiceTimerSessionBean.java
+++ src/java/org/ejbca/core/ejb/services/ServiceTimerSessionBean.java
@@ -13,6 +13,7 @@
 
 package org.ejbca.core.ejb.services;
 
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
@@ -264,7 +265,13 @@ public class ServiceTimerSessionBean extends BaseSessionBean implements javax.ej
 		HashSet existingTimers = new HashSet();
 		while(iter.hasNext()){
 			Timer timer = (Timer) iter.next();
-			existingTimers.add(timer.getInfo());    			
+			try {
+				Serializable info = timer.getInfo();
+				existingTimers.add(info);    			
+			} catch (Throwable e) {
+				// We need this try because weblogic seems to suck...
+				log.debug("Error invoking timer.getInfo(): ", e);
+			}
 		}
 
 		HashMap idToNameMap = getServiceSession().getServiceIdToNameMap(intAdmin);
