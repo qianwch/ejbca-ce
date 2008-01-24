c0d93bd43299ed8ce06d8ee340eade8c826e11a6
primelars <USER@primekey.com>
2008-01-24 19:41:17 +0000

    ECA-693: java sync improved
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_5/ejbca@5164 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/ejb/log/LocalLogSessionBean.java
+++ src/java/org/ejbca/core/ejb/log/LocalLogSessionBean.java
@@ -159,7 +159,7 @@ import org.ejbca.util.query.Query;
  * @jonas.bean
  *   ejb-name="LogSession"
  *
- * @version $Id: LocalLogSessionBean.java,v 1.19 2007-04-13 06:04:41 herrvendil Exp $
+ * @version $Id: LocalLogSessionBean.java,v 1.19.2.1 2008-01-24 19:41:17 primelars Exp $
  */
 public class LocalLogSessionBean extends BaseSessionBean {
 
@@ -201,6 +201,29 @@ public class LocalLogSessionBean extends BaseSessionBean {
     private final String LOGENTRYDATA_COL_COMMENT_OLD = "comment";
     private final String LOGENTRYDATA_COL_COMMENT_ORA = "comment_";
 
+    // object used to assure that only one thread is using a service at a time
+    private final SyncObject syncObject;
+
+    private class SyncObject {
+        boolean fIsOccupied;
+        SyncObject() {
+            fIsOccupied = false;
+        }
+        boolean isOccupied() {
+            return fIsOccupied;
+        }
+        void occupy() {
+            fIsOccupied = true;
+        }
+        void free() {
+            fIsOccupied = false;
+            notifyAll();
+        }
+    }
+    LocalLogSessionBean() {
+        super();
+        syncObject = new SyncObject();
+    }
     /**
      * Default create for SessionBean without any creation Arguments.
      */
@@ -328,17 +351,31 @@ public class LocalLogSessionBean extends BaseSessionBean {
      * Internal implementation for logging
      */
     private synchronized void doSyncronizedLog(Admin admin, int caid, int module, Date time, String username, X509Certificate certificate, int event, String comment, Exception ex) {
-        final LogConfiguration config = loadLogConfiguration(caid);
-        if (config.logEvent(event)) {
+        synchronized(syncObject) {
+            while( syncObject.isOccupied() ) {
+                try {
+                    syncObject.wait();
+                } catch (InterruptedException e) {
+                    throw new Error(e);
+                }
+            }
             try {
-                if (config.useLogDB()) {
-                    logDB(admin, caid, module, time, username, certificate, event, comment);
+                syncObject.occupy();
+                final LogConfiguration config = loadLogConfiguration(caid);
+                if (config.logEvent(event)) {
+                    try {
+                        if (config.useLogDB()) {
+                            logDB(admin, caid, module, time, username, certificate, event, comment);
+                        }
+                    } finally {
+                        // make sure to log here if the db fails
+                        if (config.useExternalLogDevices()) {
+                            logExternal(admin, caid, module, time, username, certificate, event, comment, ex);
+                        }
+                    }
                 }
             } finally {
-                // make sure to log here if the db fails
-                if (config.useExternalLogDevices()) {
-                    logExternal(admin, caid, module, time, username, certificate, event, comment, ex);
-                }
+                syncObject.free();
             }
         }
     }
