801f946ff35bf260c99641abdec299b14ef6691e
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2008-05-23 12:57:30 +0000

    ECA-808: only make info logging when totally natural "errors" happen
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_6/ejbca@5647 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/protocol/ws/EjbcaWS.java
+++ src/java/org/ejbca/core/protocol/ws/EjbcaWS.java
@@ -383,9 +383,14 @@ public class EjbcaWS implements IEjbcaWS {
 		} catch (InvalidKeyException e) {
 			log.error("EJBCA WebService error, pkcs10Req : ",e);
 			throw new EjbcaException(e.getMessage());
+		} catch (IllegalKeyException e) {
+			// Don't log a bad error for this (user's key length too small)
+			log.debug("EJBCA WebService error, pkcs12Req : ",e);
+		    throw new EjbcaException(e.getMessage());
 		} catch (AuthStatusException e) {
-			log.error("EJBCA WebService error, pkcs10Req : ",e);
-			throw new EjbcaException(e.getMessage());
+			// Don't log a bad error for this (user wrong status)
+			log.debug("EJBCA WebService error, pkcs10Req : ",e);
+		    throw new EjbcaException(e.getMessage());
 		} catch (AuthLoginException e) {
 			log.error("EJBCA WebService error, pkcs10Req : ",e);
 			throw new EjbcaException(e.getMessage());
@@ -509,13 +514,15 @@ public class EjbcaWS implements IEjbcaWS {
 				log.error("EJBCA WebService error, pkcs12Req : ",e);
 			    throw new EjbcaException(e.getMessage());
 			} catch (AuthStatusException e) {
-				log.error("EJBCA WebService error, pkcs12Req : ",e);
+				// Don't log a bad error for this (user wrong status)
+				log.debug("EJBCA WebService error, pkcs12Req : ",e);
 			    throw new EjbcaException(e.getMessage());
 			} catch (AuthLoginException e) {
 				log.error("EJBCA WebService error, pkcs12Req : ",e);
 			    throw new EjbcaException(e.getMessage());
 			} catch (IllegalKeyException e) {
-				log.error("EJBCA WebService error, pkcs12Req : ",e);
+				// Don't log a bad error for this (user's key length too small)
+				log.debug("EJBCA WebService error, pkcs12Req : ",e);
 			    throw new EjbcaException(e.getMessage());
 			} catch (CADoesntExistsException e) {
 				log.error("EJBCA WebService error, pkcs12Req : ",e);
