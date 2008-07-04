d4be991e6fe5b24bf3af00fee7faf3e345030509
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2008-07-04 13:46:47 +0000

    ECA-847: make an option in healtcheck settings to make an actual test signature with the CA token. Also optimize the CA status check.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_5/ejbca@5840 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/model/ca/caadmin/CAInfo.java
+++ src/java/org/ejbca/core/model/ca/caadmin/CAInfo.java
@@ -100,6 +100,7 @@ public class CAInfo implements Serializable {
     public int getCAId(){return this.caid;}
     public String getName() {return this.name;}
     public int getStatus() {return status;}
+    public void setStatus(int status) {this.status = status;}
     public int getCAType() {return catype;}
     public int getSignedBy() {return signedby;}
     
