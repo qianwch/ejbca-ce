c3a445e56ca79d75f3ed0f028d3a213d41a4b46f
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2009-01-28 15:42:01 +0000

    ECA-966: improve error message when entering a garbage ECDSA keyspec
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_8/ejbca@6908 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/util/keystore/KeyTools.java
+++ src/java/org/ejbca/util/keystore/KeyTools.java
@@ -121,6 +121,9 @@ public class KeyTools {
         		log.debug("Generating named curve ECDSA key pair: "+keySpec);
             	// We have EC keys
             	ecSpec = ECNamedCurveTable.getParameterSpec(keySpec);        		
+            	if (ecSpec == null) {
+            		throw new InvalidAlgorithmParameterException("keySpec "+keySpec+" is invalid for ECDSA.");
+            	}
         	}
         	keygen.initialize(ecSpec, new SecureRandom());
         } else {
