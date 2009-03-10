00bf4c47b8b3e84240a180e3e8ff59d7a58db331
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2009-03-10 19:48:16 +0000

    ECA-1141: fix correct coding of Issuing Distribution point CRL extension. Patch by Nuno Ponte of Multicert.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_8/ejbca@7088 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/model/ca/caadmin/X509CA.java
+++ src/java/org/ejbca/core/model/ca/caadmin/X509CA.java
@@ -62,6 +62,7 @@ import org.bouncycastle.asn1.x509.DistributionPoint;
 import org.bouncycastle.asn1.x509.DistributionPointName;
 import org.bouncycastle.asn1.x509.GeneralName;
 import org.bouncycastle.asn1.x509.GeneralNames;
+import org.bouncycastle.asn1.x509.IssuingDistributionPoint;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.asn1.x509.X509DefaultEntryConverter;
 import org.bouncycastle.asn1.x509.X509Extension;
@@ -105,8 +106,6 @@ import org.ejbca.core.model.ra.UserDataVO;
 import org.ejbca.util.CertTools;
 import org.ejbca.util.cert.PrintableStringEntryConverter;
 import org.ejbca.util.dn.DnComponents;
-
-import com.sun.security.auth.X500Principal;
 
 
 
@@ -715,19 +714,15 @@ public class X509CA extends CA implements Serializable {
         }
     	// CRL Distribution point URI and Freshest CRL DP
   	    if(getUseCrlDistributionPointOnCrl()) {
-  	        String crldistpoint;
-  	        if(isDeltaCRL) {
-  	            crldistpoint = getCADefinedFreshestCRL();
-  	        } else {
-  	            crldistpoint = getDefaultCRLDistPoint();
-  	        }
-
+  	        String crldistpoint = getDefaultCRLDistPoint();
   	        List distpoints = generateDistributionPoints(crldistpoint);
 
   	        if (distpoints.size() > 0) {
-  	            CRLDistPoint ext = new CRLDistPoint((DistributionPoint[])distpoints.toArray(new DistributionPoint[0]));
-  	            crlgen.addExtension(X509Extensions.CRLDistributionPoints.getId(),
-  	                                getCrlDistributionPointOnCrlCritical(), ext);
+  	            IssuingDistributionPoint idp =
+  	                new IssuingDistributionPoint(((DistributionPoint) distpoints.get(0)).getDistributionPoint(),
+  	                                             false, false, null, false, false);
+  	            crlgen.addExtension(X509Extensions.IssuingDistributionPoint.getId(),
+  	                                getCrlDistributionPointOnCrlCritical(), idp);
   	        }
 
             if (!isDeltaCRL) {
