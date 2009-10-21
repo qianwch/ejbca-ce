8f1ac91092dfdd8c99a1298f05e68fb6e3307cca
primelars <USER@primekey.com>
2009-10-21 21:07:27 +0000

    ECA-1440: configurable output on admin page
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_9/ejbca@8155 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/config/WebConfiguration.java
+++ src/java/org/ejbca/config/WebConfiguration.java
@@ -23,4 +23,12 @@ public class WebConfiguration {
 	   	return "text/plain;charset=" + ConfigurationHolder.getString ("web.contentencoding", "UTF-8");
 	}
 
+    public static boolean doShowStackTraceOnErrorPage(){
+        final String s=ConfigurationHolder.getString ("web.errorpage.stacktrace", null);
+        return s==null || s.toLowerCase().indexOf("true")>=0;
+	}
+
+    public static String notification(String sDefault){
+        return ConfigurationHolder.getString ("web.errorpage.notification", sDefault);
+    }
 }
