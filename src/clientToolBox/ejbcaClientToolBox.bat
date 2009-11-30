59dfecda79703209bfcece25f9b3f5c00fe68bb0
dcarella <USER@primekey.com>
2009-11-30 11:15:09 +0000

    clientToolBox (batch) fixed
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_9/ejbca@8366 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/clientToolBox/ejbcaClientToolBox.bat
+++ src/clientToolBox/ejbcaClientToolBox.bat
@@ -1,9 +1,10 @@
 @echo off
 rem Starting ctb using a Windows .BAT file
-set TOOLBOX_HOME=%~dp0
-set CLASSES=%TOOLBOX_HOME%clientToolBox.jar
+rem set TOOLBOX_HOME=.
+set TOOLBOX_HOME=%EJBCA_HOME%\clientToolBox-dist
+set CLASSES=%TOOLBOX_HOME%\clientToolBox.jar
 SETLOCAL ENABLEDELAYEDEXPANSION
-FOR %%J IN ("%TOOLBOX_HOME%lib\*.jar") DO set CLASSES=!CLASSES!;%%J
+FOR %%J IN ("%TOOLBOX_HOME%\lib\*.jar") DO set CLASSES=!CLASSES!;%%J
 
 rem Fixup arguments, we have to do this since windows normally only 
 rem supports %1-%9 as command line arguments
@@ -20,4 +21,4 @@ shift
 set j=%9
 rem echo %a% %b% %c% %d% %e% %f% %g% %h% %i% %j%
 
-java -Djava.endorsed.dirs=%TOOLBOX_HOME%endorsed -cp %CLASSES% org.ejbca.ui.cli.ClientToolBox %a% %b% %c% %d% %e% %f% %g% %h% %i% %j%
+java -Djava.endorsed.dirs=%TOOLBOX_HOME%\endorsed -cp %CLASSES% org.ejbca.ui.cli.ClientToolBox %a% %b% %c% %d% %e% %f% %g% %h% %i% %j%
