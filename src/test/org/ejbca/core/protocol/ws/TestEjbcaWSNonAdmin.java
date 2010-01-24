13579f93b18de1f06d0d933e27028610e6f14647
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-01-24 17:46:51 +0000

    add EJBCA license headers
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_9/ejbca@8576 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/test/org/ejbca/core/protocol/ws/TestEjbcaWSNonAdmin.java
+++ src/test/org/ejbca/core/protocol/ws/TestEjbcaWSNonAdmin.java
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.core.protocol.ws; 
 
 import java.io.File;
@@ -42,6 +54,10 @@ import org.ejbca.core.protocol.ws.client.gen.WaitingForApprovalException_Excepti
 import org.ejbca.ui.cli.batch.BatchMakeP12;
 import org.ejbca.util.CertTools;
 
+/** 
+ * 
+ * @version $Id$
+ */
 public class TestEjbcaWSNonAdmin extends CommonEjbcaWSTest {	
     private static String adminusername1 = null;               
     private static X509Certificate admincert1 = null;        
