afc57d1e37898994881bac8932086fa9f2fe7061
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-07-06 15:56:22 +0000

    javadoc update
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_10/ejbca@9400 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/model/services/workers/CRLUpdateWorker.java
+++ src/java/org/ejbca/core/model/services/workers/CRLUpdateWorker.java
@@ -21,14 +21,11 @@ import org.ejbca.core.ejb.ca.caadmin.ICAAdminSessionLocal;
 import org.ejbca.core.ejb.ca.crl.ICreateCRLSessionLocal;
 import org.ejbca.core.ejb.ca.crl.ICreateCRLSessionLocalHome;
 import org.ejbca.core.model.InternalResources;
-import org.ejbca.core.model.SecConst;
 import org.ejbca.core.model.services.BaseWorker;
 import org.ejbca.core.model.services.ServiceExecutionFailedException;
 
 /**
- * Class managing the updating of CRLs.
- * 
- * This is a replacement of the old jboss service.
+ * Class managing the updating of CRLs. Loops through the list of CAs to check and generates CRLs and deltaCRLs if needed.
  * 
  * @author Philip Vendil
  * @version $Id$
@@ -41,6 +38,9 @@ public class CRLUpdateWorker extends BaseWorker {
 
     private ICreateCRLSessionLocal createcrlsession = null;
 
+    /** Semaphore that tries to make sure that this CRL creation job does not run several times on the same machine.
+     * Since CRL generation can sometimes take a lot of time, this is needed.
+     */
 	private static boolean running = false;
 
 	/**
