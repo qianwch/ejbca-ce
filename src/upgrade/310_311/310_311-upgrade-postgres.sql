e7852c59c84a2abce28d6fe76dc298c490844e21
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-09-27 07:47:40 +0000

    ECA-1847: database upgrades to harmonize data types between all databases. Brings database in sync with what EJBCA 4.0/hibernate wants. Fix SqlExecutor to accept sql comments.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_11/ejbca@9998 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ src/upgrade/310_311/310_311-upgrade-postgres.sql
@@ -0,0 +1,17 @@
+
+-- BOOLEANs on WebSphere were created as INT2 instead of "BOOLEAN". Hibernate will map this as BOOLEAN so we probably need to update this. 
+--  We cannot cast INT2 to BOOLEAN so we have to work around it..
+-- NOTE! This is only for WebSphere, not other application servers. Therefore we do not run them by default
+--ALTER TABLE AccessRulesData ADD tmp BOOLEAN DEFAULT FALSE NOT NULL;
+--UPDATE AccessRulesData SET tmp=TRUE WHERE isRecursive=1;
+--ALTER TABLE AccessRulesData DROP isRecursive;
+--ALTER TABLE AccessRulesData ADD isRecursive BOOLEAN DEFAULT FALSE NOT NULL;
+--UPDATE AccessRulesData SET isRecursive=tmp;
+--ALTER TABLE AccessRulesData DROP tmp;
+
+--ALTER TABLE KeyRecoveryData ADD tmp BOOLEAN DEFAULT FALSE NOT NULL;
+--UPDATE KeyRecoveryData SET tmp=TRUE WHERE markedAsRecoverable=1;
+--ALTER TABLE KeyRecoveryData DROP markedAsRecoverable;
+--ALTER TABLE KeyRecoveryData ADD markedAsRecoverable BOOLEAN DEFAULT FALSE NOT NULL;
+--UPDATE KeyRecoveryData SET markedAsRecoverable=tmp;
+--ALTER TABLE KeyRecoveryData DROP tmp;
