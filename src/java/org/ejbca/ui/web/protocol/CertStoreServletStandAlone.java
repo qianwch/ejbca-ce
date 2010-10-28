c6918b03d12bb072f462dc5f3324751e6d07bfce
primelars <USER@primekey.com>
2010-10-28 15:27:13 +0000

    ECA-1853: added class not implemented yet to test the build.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_11/ejbca@10325 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ src/java/org/ejbca/ui/web/protocol/CertStoreServletStandAlone.java
@@ -0,0 +1,49 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.ejbca.ui.web.protocol;
+
+import org.ejbca.core.protocol.ocsp.CertStoreStandAlone;
+
+
+/** 
+ * Servlet implementing server side of the Certificate Store.
+ * For a detailed description see rfc4378.
+ * 
+ * @web.servlet name = "CertificateStore"
+ *              display-name = "CertStoreServletStandAlone"
+ *              description="Fetches certificates according to rfc4378"
+ *              load-on-startup = "99"
+ *
+ * @web.servlet-mapping url-pattern = "/search.cgi"
+ *
+ * @web.ejb-local-ref
+ *  name="ejb/CertificateStoreOnlyDataSessionLocal"
+ *  type="Session"
+ *  link="CertificateStoreOnlyDataSession"
+ *  home="org.ejbca.core.ejb.ca.store.ICertificateStoreOnlyDataSessionLocalHome"
+ *  local="org.ejbca.core.ejb.ca.store.ICertificateStoreOnlyDataSessionLocal"
+ *
+ * @web.resource-ref
+ *  name="${datasource.jndi-name-prefix}${datasource.jndi-name}"
+ *  type="javax.sql.DataSource"
+ *  auth="Container"
+ *
+ * @author Lars Silven PrimeKey
+ * @version  $Id$
+ */
+public class CertStoreServletStandAlone extends CertStoreServletBase {
+    public CertStoreServletStandAlone() {
+        super( new CertStoreStandAlone() );
+    }
+} // CertStoreServletStandAlone
