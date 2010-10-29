8c7511cac4036bec9cab669da57c3ead2f4b0a28
primelars <USER@primekey.com>
2010-10-29 15:18:08 +0000

    ECA-1853: Downloading of multiple certificate now working (test) Verified with Firefox (creates different files for each cert) and wget (gets search.cgi with the mutlipart/mixed body).
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_11/ejbca@10331 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/ui/web/protocol/CertStoreServletBase.java
+++ src/java/org/ejbca/ui/web/protocol/CertStoreServletBase.java
@@ -13,8 +13,19 @@
 
 package org.ejbca.ui.web.protocol;
 
-import javax.servlet.http.HttpServlet;
+import java.io.PrintStream;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
+import java.util.Iterator;
+import java.util.Random;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.ejbca.core.model.SecConst;
+import org.ejbca.core.model.log.Admin;
 import org.ejbca.core.protocol.ocsp.ICertStore;
 
 /**
@@ -22,6 +33,8 @@ import org.ejbca.core.protocol.ocsp.ICertStore;
  * @version  $Id$
  */
 class CertStoreServletBase extends HttpServlet {
+    private final Admin admin = new Admin(Admin.TYPE_INTERNALUSER);
+    private final static String BOUNDARY = "\"BOUNDARY\"";
 
     private final ICertStore certStore;
     /**
@@ -29,5 +42,32 @@ class CertStoreServletBase extends HttpServlet {
      */
     CertStoreServletBase(ICertStore _certStore ) {
         this.certStore = _certStore;
+    }
+    /* (non-Javadoc)
+     * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, java.io.IOException {
+        resp.setContentType("multipart/mixed; boundary="+BOUNDARY);
+        final PrintStream ps = new PrintStream(resp.getOutputStream());
+        ps.println("This is a multi-part message in MIME format.");
+        final Random r = new Random();
+        final Iterator i = this.certStore.findCertificatesByType(this.admin, SecConst.CERTTYPE_SUBCA, null).iterator();
+        while( i.hasNext() ) {
+            final X509Certificate cert = (X509Certificate)i.next();
+            // Upload the certificates with mime-header for user certificates.
+            ps.println("--"+BOUNDARY);
+            ps.println("Content-type: application/pkix-cert");
+            ps.println("Content-disposition: attachment; filename=cert" + r.nextInt(1000) + ".der");
+            ps.println();
+            try {
+                ps.write(cert.getEncoded());
+            } catch (CertificateEncodingException e) {
+                throw new ServletException(e);
+            }
+            ps.println();
+        }
+        // ready
+        ps.println("--"+BOUNDARY+"--");
+        ps.flush();
     }
 }
