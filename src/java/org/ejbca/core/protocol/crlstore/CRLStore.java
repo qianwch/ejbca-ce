c512d9a9e2170e24c1e047b532d723d39528d767
primelars <USER@primekey.com>
2010-11-14 12:06:49 +0000

    ECA-1853: removed @ that the preprocessing don't like.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_11/ejbca@10482 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/protocol/crlstore/CRLStore.java
+++ src/java/org/ejbca/core/protocol/crlstore/CRLStore.java
@@ -17,7 +17,6 @@ import javax.ejb.EJBException;
 import org.ejbca.core.ejb.ServiceLocator;
 import org.ejbca.core.ejb.ca.crl.ICreateCRLSessionLocal;
 import org.ejbca.core.ejb.ca.crl.ICreateCRLSessionLocalHome;
-import org.ejbca.core.ejb.ca.store.ICertificateStoreSessionLocalHome;
 import org.ejbca.core.model.ca.store.CRLInfo;
 import org.ejbca.core.model.log.Admin;
 
@@ -32,7 +31,7 @@ public class CRLStore implements ICRLStore {
 
     private static ICreateCRLSessionLocal crlStore = null;
     synchronized ICreateCRLSessionLocal getCRLSession(){
-        if(crlStore == null){    
+        if(crlStore == null){   
             try {
             	ICreateCRLSessionLocalHome storehome = (ICreateCRLSessionLocalHome)ServiceLocator.getInstance().getLocalHome(ICreateCRLSessionLocalHome.COMP_NAME);
             	crlStore = storehome.create();
@@ -45,30 +44,25 @@ public class CRLStore implements ICRLStore {
 	/* (non-Javadoc)
 	 * @see org.ejbca.core.protocol.crlstore.ICRLStore#getCRLInfo(org.ejbca.core.model.log.Admin, java.lang.String)
 	 */
-	@Override
 	public CRLInfo getCRLInfo(Admin admin, String fingerprint) {
 		return getCRLSession().getCRLInfo(admin, fingerprint);
 	}
 	/* (non-Javadoc)
 	 * @see org.ejbca.core.protocol.crlstore.ICRLStore#getLastCRL(org.ejbca.core.model.log.Admin, java.lang.String, boolean)
 	 */
-	@Override
 	public byte[] getLastCRL(Admin admin, String issuerdn, boolean deltaCRL) {
 		return getCRLSession().getLastCRL(admin, issuerdn, deltaCRL);
 	}
 	/* (non-Javadoc)
 	 * @see org.ejbca.core.protocol.crlstore.ICRLStore#getLastCRLInfo(org.ejbca.core.model.log.Admin, java.lang.String, boolean)
 	 */
-	@Override
 	public CRLInfo getLastCRLInfo(Admin admin, String issuerdn, boolean deltaCRL) {
 		return getCRLSession().getLastCRLInfo(admin, issuerdn, deltaCRL);
 	}
 	/* (non-Javadoc)
 	 * @see org.ejbca.core.protocol.crlstore.ICRLStore#getLastCRLNumber(org.ejbca.core.model.log.Admin, java.lang.String, boolean)
 	 */
-	@Override
 	public int getLastCRLNumber(Admin admin, String issuerdn, boolean deltaCRL) {
 		return getCRLSession().getLastCRLNumber(admin, issuerdn, deltaCRL);
 	}
-
 }
