92f819b3b7fc20cdd5297d05172a985e89d5f534
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-11-18 12:03:02 +0000

    ECA-1901: make default value empty string and make sure empty or null values are always denied, that's a secure default.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_11/ejbca@10565 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src/org/ejbca/core/ejb/ca/caadmin/TestCAs.java
+++ modules/systemtests/src/org/ejbca/core/ejb/ca/caadmin/TestCAs.java
@@ -185,6 +185,11 @@ public class TestCAs extends TestCase {
             byte[] request = TestTools.getCAAdminSession().makeRequest(admin, info.getCAId(), cachain, false, false, false, null);
             PKCS10RequestMessage msg = new PKCS10RequestMessage(request);
             assertEquals("CN=TEST", msg.getRequestDN());
+            
+            // Check CMP RA sercret, default value empty string
+            X509CAInfo xinfo = (X509CAInfo)info;
+            assertNotNull(xinfo.getCmpRaAuthSecret());
+            assertEquals("", xinfo.getCmpRaAuthSecret());
         } catch (CAExistsException pee) {
             log.info("CA exists.");
         }
