9be62e50d16af0662deda62faccb3075d05d0b2f
Johan Eklund <johan.eklund@primekey.com>
2010-11-22 20:13:49 +0000

    ECA-1924: Corrected rowProtection column upgrade on MS SQL.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_11/ejbca@10645 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/upgrade/310_311/310_311-post-upgrade-mssql2000.sql
+++ src/upgrade/310_311/310_311-post-upgrade-mssql2000.sql
@@ -1,6 +1,6 @@
 -- Add rowVersion column to all tables
 -- PublisherQueueData was a late add-on so we need to check if the column was created during appserver start-up
-ALTER TABLE PublisherQueueData ADD COLUMN rowVersion INTEGER NOT NULL DEFAULT '0';
+ALTER TABLE PublisherQueueData ADD rowVersion INTEGER NOT NULL DEFAULT '0';
 
 -- Add rowProtection column to all tables
-ALTER TABLE PublisherQueueData ADD COLUMN rowProtection TEXT DEFAULT NULL;
+ALTER TABLE PublisherQueueData ADD rowProtection TEXT DEFAULT NULL;
