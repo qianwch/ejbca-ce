33e2a44ceecf7c71072b6de1e77d1734641f3e61
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2010-11-25 12:38:26 +0000

    ECA-1853: add EJBCA license and version headers.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_11/ejbca@10690 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/protocol/ocsp/standalonesession/SigningEntity.java
+++ src/java/org/ejbca/core/protocol/ocsp/standalonesession/SigningEntity.java
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.core.protocol.ocsp.standalonesession;
 
 import java.security.PrivateKey;
@@ -14,6 +26,7 @@ import org.ejbca.core.protocol.ocsp.OCSPUtil;
 
 /**
  * An object of this class is used to sign OCSP responses for certificates belonging to one CA.
+ * @version $Id$
  */
 class SigningEntity {
     /**
