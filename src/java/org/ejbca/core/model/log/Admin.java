125e5056b1483b0722216c70b2dc0db06a54537d
netmackan <USER@primekey.com>
2011-03-30 13:58:26 +0000

    ECA-2018: Not serializing authtoken. Restore authtoken when deserializing approval data.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_11/ejbca@11639 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/core/model/log/Admin.java
+++ src/java/org/ejbca/core/model/log/Admin.java
@@ -77,7 +77,8 @@ public class Admin implements Serializable {
     protected String username = null;
     protected String email = null;
     
-    protected byte[] authToken = AdminInformation.getRandomToken();
+    /** transient authToken should _not_ be serialized. **/
+    protected transient byte[] authToken = AdminInformation.getRandomToken();
 	
     // Public Constructors
     public Admin(Certificate certificate, String username, String email) {
@@ -156,5 +157,17 @@ public class Admin implements Serializable {
      */
     public String getUsername() {
     	return username;
+    }
+    
+    /**
+     * Manually sets the authToken. This should only be done in special cases 
+     * such as when restoring an Approval from the database.
+     * Note: Setting this to AdminInformation.getRandomToken() means that 
+     * this object is treated as it were created internal in EJBCA. Do not do
+     * that unless trusting the object!
+     * @param authToken Value of the authtoken.
+     */
+    public void setAuthToken(final byte[] authToken) {
+    	this.authToken = authToken;
     }
 }
