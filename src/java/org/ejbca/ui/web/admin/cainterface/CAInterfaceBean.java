efcaed7ed5d2c1ea22a4a0ee6f89995e2f3b0972
Johan Eklund <johan.eklund@primekey.com>
2011-04-07 16:56:33 +0000

    ECA-2135: Allow republish of certificate with special chars in issuer DN from Admin GUI.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_3_11/ejbca@11705 37aeb1de-b14b-0410-9a0e-82b28170d228

--- src/java/org/ejbca/ui/web/admin/cainterface/CAInterfaceBean.java
+++ src/java/org/ejbca/ui/web/admin/cainterface/CAInterfaceBean.java
@@ -377,7 +377,8 @@ public class CAInterfaceBean implements java.io.Serializable {
 	   String password = null;
 	   String dn = null;
 	   ExtendedInformation ei = null;
-	   final CertReqHistory certreqhist = certificatesession.getCertReqHistory(administrator, certificatedata.getSerialNumberBigInt(), certificatedata.getIssuerDN());
+	   final Certificate certificate = certificatedata.getCertificate();
+	   final CertReqHistory certreqhist = certificatesession.getCertReqHistory(administrator, CertTools.getSerialNumber(certificate), CertTools.getIssuerDN(certificate));
 	   if (certreqhist != null) {
 		   // First try to look up all info using the Certificate Request History from when the certificate was issued
 		   // We need this since the certificate subjectDN might be a subset of the subjectDN in the template
@@ -387,7 +388,7 @@ public class CAInterfaceBean implements java.io.Serializable {
 		   dn = certreqhist.getUserDataVO().getDN();
 		   ei = certreqhist.getUserDataVO().getExtendedinformation();
 	   }
-	   final CertificateInfo certinfo = certificatesession.getCertificateInfo(administrator, CertTools.getFingerprintAsString(certificatedata.getCertificate()));
+	   final CertificateInfo certinfo = certificatesession.getCertificateInfo(administrator, CertTools.getFingerprintAsString(certificate));
 	   if (certinfo != null) {
 		   // If we are missing Certificate Request History for this certificate, we can at least recover some of this info
 		   if (certificateProfileId == SecConst.CERTPROFILE_NO_PROFILE) {
