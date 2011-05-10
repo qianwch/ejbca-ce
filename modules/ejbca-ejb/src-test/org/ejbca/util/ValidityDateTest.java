5819c202a79e7aef9cbb9be5eeb83e409d5f894a
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2011-05-10 11:29:25 +0000

    ECA-2006: add EJBCA and svn Id headers
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_4_0/ejbca@11935 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb/src-test/org/ejbca/util/ValidityDateTest.java
+++ modules/ejbca-ejb/src-test/org/ejbca/util/ValidityDateTest.java
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.ejbca.util;
 
 import java.util.Calendar;
@@ -5,6 +17,9 @@ import java.util.Date;
 
 import junit.framework.TestCase;
 
+/**
+ * @version $Id$
+ */
 public class ValidityDateTest extends TestCase {
     public ValidityDateTest(String name) { super(name); }
 
