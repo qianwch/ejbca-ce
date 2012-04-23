501c615960dc4dc642e26dda38c5a80e3da6783f
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2012-04-23 10:27:23 +0000

    ECA-2662: strip whitespace from entered username in public web.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_4_0/ejbca@14581 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/publicweb-gui/src/org/ejbca/ui/web/pub/RequestInstance.java
+++ modules/publicweb-gui/src/org/ejbca/ui/web/pub/RequestInstance.java
@@ -106,7 +106,7 @@ public class RequestInstance {
 		try {
 			setParameters(request);
 
-			String username = getParameter("user");
+			final String username = StringUtils.strip(getParameter("user"));
 			String password = getParameter("password");
 			String keylengthstring = getParameter("keylength");
 			String keyalgstring = getParameter("keyalg");
