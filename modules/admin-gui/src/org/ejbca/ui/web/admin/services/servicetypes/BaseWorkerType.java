ec8db674b8f0299d0dee999561ba437372a999f7
Mike Agrenius Kushner <mike.kushner@primekey.com>
2012-07-02 16:52:16 +0000

    ECA-2706: Added header and keyword
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/Branch_4_0/ejbca@15091 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/services/servicetypes/BaseWorkerType.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/services/servicetypes/BaseWorkerType.java
@@ -1,3 +1,16 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
 package org.ejbca.ui.web.admin.services.servicetypes;
 
 import java.io.IOException;
@@ -9,6 +22,13 @@ import java.util.Properties;
 
 import org.ejbca.core.model.services.BaseWorker;
 import org.ejbca.core.model.services.IWorker;
+
+/**
+ * Base type for workers
+ * 
+ * @version $Id$
+ *
+ */
 
 public abstract class BaseWorkerType extends WorkerType {
 
