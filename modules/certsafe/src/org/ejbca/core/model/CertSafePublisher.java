02a864b33af83d087682ba6b9325bd57643d763e
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2014-08-27 15:58:34 +0000

    ECA-3437: improve log message and unknown host message, change some errors to info
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/primekey/Branch_6_2/ejbca@19607 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/certsafe/src/org/ejbca/core/model/CertSafePublisher.java
+++ modules/certsafe/src/org/ejbca/core/model/CertSafePublisher.java
@@ -19,6 +19,7 @@ import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
@@ -257,11 +258,13 @@ public class CertSafePublisher implements ICustomPublisher {
         }
         checkProperties();
         
-        AuthenticationToken admin = new AlwaysAllowLocalAuthenticationToken(new UsernamePrincipal("Test SSL Connection"));
+        AuthenticationToken admin = new AlwaysAllowLocalAuthenticationToken(new UsernamePrincipal("Test CertSafe Connection"));
         
         SSLSocketFactory sslSocketFactory = getSSLSocketFactory(admin);
         try {
-            log.info("https URL: " + urlstr);
+            if (log.isDebugEnabled()) {
+                log.info("CertSafe https URL: " + urlstr);
+            }
             URL url = new URL(urlstr);
             HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
             con.setSSLSocketFactory(sslSocketFactory);
@@ -271,19 +274,23 @@ public class CertSafePublisher implements ICustomPublisher {
             con.setConnectTimeout(timeout);
             con.connect();
             if(con.getResponseCode() != 200) {
-                throw new PublisherConnectionException("Testing connection failed");
+                throw new PublisherConnectionException("Testing connection failed, response code: "+con.getResponseCode());
             }
+        } catch (UnknownHostException e) {
+            String msg = "Unknown host: "+e.getLocalizedMessage();
+            log.info(msg, e);
+            throw new PublisherConnectionException(msg); 
         } catch (MalformedURLException e) {
             String msg = e.getLocalizedMessage();
-            log.error(msg, e);
+            log.info(msg, e);
             throw new PublisherConnectionException(msg); 
         } catch (ProtocolException e) {
             String msg = e.getLocalizedMessage();
-            log.error(msg, e);
+            log.info(msg, e);
             throw new PublisherConnectionException(msg);
         } catch (IOException e) {
             String msg = e.getLocalizedMessage();
-            log.error(msg, e);
+            log.info(msg, e);
             throw new PublisherConnectionException(msg);
         }
     }
