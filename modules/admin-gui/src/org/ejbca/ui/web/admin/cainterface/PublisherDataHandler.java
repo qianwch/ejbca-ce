b52726f1781291c4998464f60df9d87c9ecd135f
Tomas Gustavsson <tomas.gustavsson@primekey.com>
2015-05-19 16:28:29 +0000

    ECA-4226: Make sure we don't remove publisher if there are any references left
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/primekey/Branch_6_2_6_x/ejbca@21303 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/cainterface/PublisherDataHandler.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/cainterface/PublisherDataHandler.java
@@ -82,7 +82,7 @@ public class PublisherDataHandler implements Serializable {
 
       if(authorizedToEditPublishers()){
       	int publisherid = publishersession.getPublisherId(name);
-        if(!caadminsession.exitsPublisherInCAs(administrator, publisherid) && !certificateProfileSession.existsPublisherIdInCertificateProfiles(publisherid)){      	
+        if(!caadminsession.exitsPublisherInCAs(publisherid) && !certificateProfileSession.existsPublisherIdInCertificateProfiles(publisherid)){      	
 		  publishersession.removePublisher(administrator, name);
 		  this.info.publishersEdited();
 		  returnval = false;
