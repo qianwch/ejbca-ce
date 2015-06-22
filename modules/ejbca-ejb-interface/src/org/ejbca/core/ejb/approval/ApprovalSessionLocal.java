8ff2875e2a12a5b2e32bb2f4ffea481ca9eb1ac2
aveen4711 <USER@primekey.com>
2015-06-22 08:33:14 +0000

    Corrected typos in the javadoc
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/primekey/Branch_6_2/ejbca@21503 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/approval/ApprovalSessionLocal.java
+++ modules/ejbca-ejb-interface/src/org/ejbca/core/ejb/approval/ApprovalSessionLocal.java
@@ -35,17 +35,19 @@ public interface ApprovalSessionLocal extends ApprovalSession {
     /**
      * Method used to reject an approval requests.
      * 
-     * It does the follwing 1. checks if the approval with the status waiting
+     * It does the following:
+     * 
+     * 1. checks if the approval with the status waiting
      * exists, throws an ApprovalRequestDoesntExistException otherwise
      * 
-     * 2. check if the administrator is authorized using the follwing rules: 2.1
+     * 2. check if the administrator is authorized using the following rules: 2.1
      * if getEndEntityProfile is ANY_ENDENTITYPROFILE then check if the admin is
-     * authorized to AccessRulesConstants.REGULAR_APPROVECAACTION othervise
+     * authorized to AccessRulesConstants.REGULAR_APPROVECAACTION otherwise
      * AccessRulesConstants.REGULAR_APPORVEENDENTITY and APPROVAL_RIGHTS for the
-     * end entity profile. 2.2 Checks if the admin is authoried to the approval
+     * end entity profile. 2.2 Checks if the admin is authorized to the approval
      * requests getCAId()
      * 
-     * 3. looks upp the username of the administrator and checks that no
+     * 3. looks up the username of the administrator and checks that no
      * approval have been made by this user earlier.
      * 
      * 4. Runs the approval command in the end entity bean.
