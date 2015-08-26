819363b9a815745041242a4ae98dd53e77cd3327
Mike Agrenius Kushner <mike.kushner@primekey.com>
2015-08-26 13:51:14 +0000

    ECA-4381: Corrected fix for SERIALNUMBER/SN issue to use CertTools method instead.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/primekey/Branch_6_2/ejbca@21748 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cesecore-ejb/src/org/cesecore/certificates/ocsp/OcspResponseGeneratorSessionBean.java
+++ modules/cesecore-ejb/src/org/cesecore/certificates/ocsp/OcspResponseGeneratorSessionBean.java
@@ -160,6 +160,7 @@ import org.cesecore.keys.token.p11.Pkcs11SlotLabelType;
 import org.cesecore.keys.util.KeyTools;
 import org.cesecore.util.CeSecoreNameStyle;
 import org.cesecore.util.CertTools;
+import org.cesecore.util.StringTools;
 import org.cesecore.util.log.ProbableErrorHandler;
 import org.cesecore.util.log.SaferAppenderListener;
 import org.cesecore.util.log.SaferDailyRollingFileAppender;
@@ -652,11 +653,12 @@ public class OcspResponseGeneratorSessionBean implements OcspResponseGeneratorSe
             }
         } else {
             if (log.isDebugEnabled()) {
-                log.debug("Requestor name is: " + new X500Name(new CeSecoreNameStyle(), ocspRequest.getRequestorName().getName().toString()).toString());
+                String requestor = CertTools.stringToBCDNString( ocspRequest.getRequestorName().getName().toString());
+                log.debug("Requestor name is: " + requestor);
             }
             if (transactionLogger.isEnabled()) {
-                String requestorName = new X500Name(new CeSecoreNameStyle(), ocspRequest.getRequestorName().getName().toString()).toString();
-                transactionLogger.paramPut(TransactionLogger.REQ_NAME, requestorName);
+                String requestor = CertTools.stringToBCDNString( ocspRequest.getRequestorName().getName().toString());
+                transactionLogger.paramPut(TransactionLogger.REQ_NAME, requestor);
             }
         }
 
