89e0adee459a29d66b653009f88f7bc101bee450
Bastian Fredriksson <bastian.fredriksson@primekey.com>
2017-11-22 11:58:16 +0000

    NoTicket: Removed unused package.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/primekey/Branch_6_10_x/ejbca@27284 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/certprof/CertProfileBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/certprof/CertProfileBean.java
@@ -24,7 +24,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
