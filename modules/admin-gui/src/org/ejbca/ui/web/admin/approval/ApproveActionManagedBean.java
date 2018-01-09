5fb57bf5b2f139395ba426c6b8f01cf593350c21
Mike Agrenius Kushner <mike.kushner@primekey.com>
2018-01-09 11:51:25 +0000

    ECA-6490: Oddly enough the CA GUI was reading stale data when analyzing approval profile rights
    git-svn-id: https://svn.cesecore.eu/svn/ejbca-restricted/branches/primekey/Branch_6_10_x/ejbca@27803 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/admin-gui/src/org/ejbca/ui/web/admin/approval/ApproveActionManagedBean.java
+++ modules/admin-gui/src/org/ejbca/ui/web/admin/approval/ApproveActionManagedBean.java
@@ -442,19 +442,23 @@ public class ApproveActionManagedBean extends BaseManagedBean {
     }
     
     /**
-     * 
-     * @return all partitions that the current admin has view access to
-     */
+    *
+    * @return all partitions that the current admin has view access to
+    */
     public ListDataModel<ApprovalPartitionProfileGuiObject> getApprovalPartitions() {
         if (partitionsAuthorizedToView == null) {
             List<ApprovalPartitionProfileGuiObject> authorizedPartitions = new ArrayList<>();
             partitionsAuthorizedToApprove = new HashSet<>();
+            //Make sure we're not reading stale data
+            ApprovalProfile approvalProfile = approvalProfileSession.getApprovalProfile(approvalDataVOView.getApprovalProfile().getProfileId());
+            ApprovalStep approvalStep = approvalProfile.getStep(getCurrentStep().getStepIdentifier());
             if (getCurrentStep() != null) {
-                for (ApprovalPartition approvalPartition : getCurrentStep().getPartitions().values()) {
+                for (Integer approvalPartitionId : getCurrentStep().getPartitions().keySet()) {
+                    ApprovalPartition approvalPartition = approvalStep.getPartition(approvalPartitionId);
                     try {
                         if (approvalDataVOView.getApprovalProfile().canViewPartition(getAdmin(), approvalPartition)) {
-                            authorizedPartitions
-                                    .add(new ApprovalPartitionProfileGuiObject(approvalDataVOView.getApprovalProfile().getApprovalProfileTypeIdentifier(),
+                            authorizedPartitions.add(
+                                    new ApprovalPartitionProfileGuiObject(approvalDataVOView.getApprovalProfile().getApprovalProfileTypeIdentifier(),
                                             approvalPartition.getPartitionIdentifier(), getPartitionProperties(approvalPartition)));
                         }
                         if (approvalDataVOView.getApprovalProfile().canApprovePartition(getAdmin(), approvalPartition)) {
